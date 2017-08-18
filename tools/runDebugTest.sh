50c438f6a8d8a33a4793a64e8db83f6709b8f20c
Baris Aktemur <baris.aktemur@ozyegin.edu.tr>
2017-08-18 15:36:48 +0300

    Script for debug run.

---
+++ tools/runDebugTest.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+if [ "$#" -lt 1 ]; then
+  echo "Usage: $0 '<sptrsv parameters>'" >&2
+  exit 1
+fi
+
+if [ -z ${MATRICES+x} ]; then
+    echo "Set MATRICES variable to the matrices folder."
+    exit 1
+fi
+
+source /opt/intel/bin/compilervars.sh intel64
+
+methodName=$1
+param1=$2
+param2=$3
+param3=$4
+param4=$5
+
+echo "Running SpTRSV test " $methodName $param1 $param2
+
+while read line
+do
+    IFS=' ' read -r -a info <<< "$line"
+    groupName=${info[0]}
+    matrixName=${info[1]}
+
+    echo ----- "$groupName"/"$matrixName"
+
+    cd ..
+    ./build/sptrsv $MATRICES/$groupName/$matrixName/$matrixName".mtx" $methodName -d --iters=1 $param1 $param2 $param3 $param4
+    cd tools
+
+done < matrixNames.txt
+
+echo " "
